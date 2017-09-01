7417de7efb33f8239e31ff26a542f2432537efb4
Takahiro Ooishi <taka0125@gmail.com>
2017-09-02 00:02:39 +0900

    :+1: add hash

--- lib/moromi/aws/sns/message/parameter.rb
+++ lib/moromi/aws/sns/message/parameter.rb
@@ -7,19 +7,35 @@ module Moromi
         class Parameter
           # @param [Moromi::Aws::Sns::Message::Apns] apns
           # @param [Moromi::Aws::Sns::Message::Fcm] gcm
-          def initialize(default: '', apns: nil, gcm: nil)
-            @default = default
+          def initialize(apns: nil, gcm: nil)
             @apns = apns
             @gcm = gcm
           end
 
+          def to_hash
+            {
+              apns: {name: @apns.class.name, data: @apns.to_hash},
+              gcm: {name: @gcm.class.name, data: @gcm.to_hash}
+            }
+          end
+
           def to_json
             {
-              'default': @default,
+              'default': '',
               'APNS_SANDBOX': @apns&.to_message_json,
               'APNS': @apns&.to_message_json,
               'GCM': @gcm&.to_message_json
             }.compact.to_json
+          end
+
+          def self.build_from_hash(hash)
+            apns_klass = hash[:apns][:name].safe_constantize
+            apns = apns_klass.build_from_hash(hash[:apns][:data])
+
+            gcm_klass = hash[:gcm][:name].safe_constantize
+            gcm = gcm_klass.build_from_hash(hash[:gcm][:data])
+
+            new(apns: apns, gcm: gcm)
           end
         end
       end
--- moromi-aws-sns.gemspec
+++ moromi-aws-sns.gemspec
@@ -19,7 +19,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.required_ruby_version = '>= 2.2'
+
   spec.add_dependency 'aws-sdk', '~> 2'
+  spec.add_dependency 'activesupport', ['>= 4.2']
 
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
