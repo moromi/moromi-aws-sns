147f77077aff6e952f2bc7732d918352dec5e349
Takahiro Ooishi <taka0125@gmail.com>
2019-09-20 10:10:37 +0900

    :+1: content_available => push_type

--- lib/moromi/aws/sns/message/apns.rb
+++ lib/moromi/aws/sns/message/apns.rb
@@ -3,22 +3,24 @@ module Moromi
     module Sns
       module Message
         class Apns < Base
+          PUSH_TYPE_ALERT = 1
+          PUSH_TYPE_BACKGROUND = 2
+
           attr_reader :alert
           attr_reader :badge
           attr_reader :sound
-          attr_reader :content_available
           attr_reader :mutable_content
           attr_reader :category
           attr_reader :priority
           attr_reader :type
           attr_reader :custom_data
 
-          def initialize(alert:, badge:, sound: 'default', content_available: 1, mutable_content: 0, category: nil,
+          def initialize(alert:, badge:, sound: 'default', push_type: PUSH_TYPE_ALERT, mutable_content: 0, category: nil,
                          priority: 10, type: nil, custom_data: {})
             @alert = alert
             @badge = badge
             @sound = sound
-            @content_available = content_available
+            @push_type = normalize_push_type(push_type)
             @mutable_content = mutable_content
             @category = category
             @priority = priority
@@ -31,7 +33,7 @@ module Moromi
               alert: @alert,
               badge: @badge,
               sound: @sound,
-              content_available: @content_available,
+              push_type: @push_type,
               mutable_content: @mutable_content,
               category: @category,
               priority: @priority,
@@ -43,9 +45,21 @@ module Moromi
           def to_message_json
             aps = to_hash
             %i[custom_data type content_available mutable_content].each { |k| aps.delete(k) }
-            aps['content-available'] = @content_available
+            aps['content-available'] = content_available
             aps['mutable-content'] = @mutable_content
             @custom_data.merge({aps: aps}).to_json
+          end
+
+          # https://docs.aws.amazon.com/ja_jp/sns/latest/dg/sns-send-custom-platform-specific-payloads-mobile-devices.html#mobile-push-send-message-apns-background-notification
+          def content_available
+            case @push_type
+            when PUSH_TYPE_ALERT
+              'alert'
+            when PUSH_TYPE_BACKGROUND
+              1
+            else
+              'alert'
+            end
           end
 
           private
@@ -54,9 +68,20 @@ module Moromi
             custom_data
           end
 
+          def normalize_push_type(value)
+            case value
+            when PUSH_TYPE_ALERT
+              PUSH_TYPE_ALERT
+            when PUSH_TYPE_BACKGROUND
+              PUSH_TYPE_BACKGROUND
+            else
+              PUSH_TYPE_ALERT
+            end
+          end
+
           class << self
             def build_silent_push_message(priority: 10, custom_data: {})
-              new('', nil, sound: nil, content_available: 1, priority: priority, custom_data: custom_data)
+              new('', nil, sound: nil, push_type: PUSH_TYPE_BACKGROUND, priority: priority, custom_data: custom_data)
             end
           end
         end
